d60974268df5c2a7b54cbd43d13b46539b8d8657
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-11-12 18:44:13 +0700

    - Import Vector Asset SVG - Create drawable image - Add splash screen activity

---
+++ app/src/main/java/com/enigmacamp/goldmarket/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.enigmacamp.goldmarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
