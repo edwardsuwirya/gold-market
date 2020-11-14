9cdbf0b2ae31b0d01cc7264b1ba86f1880ea4fbe
Edi Murwanto <edhi.uchiha@gmail.com>
2020-11-14 10:47:40 +0700

    add string and color for bottom navigation menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.3")
     implementation("com.google.android.material:material:$material_version")
+    implementation("com.android.support:design:29.0.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
