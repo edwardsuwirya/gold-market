e798f3450b41e424a0a3dc781286267e0c001e4c
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-11-14 11:03:09 +0700

    Linear Layout, Constraint Layout

--- app/src/main/java/com/enigmacamp/goldmarket/SplashScreenActivity.kt
+++ app/src/main/java/com/enigmacamp/goldmarket/SplashScreenActivity.kt
@@ -1,11 +1,25 @@
 package com.enigmacamp.goldmarket
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        GlobalScope.launch {
+            delay(2000)
+            onStartWelcomeActivity()
+        }
+    }
+
+    fun onStartWelcomeActivity() {
+        startActivity(Intent(this, WelcomeActivity::class.java))
+        finish()
     }
 }
