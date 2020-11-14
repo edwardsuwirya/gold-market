af00d551b9f605bbf17a2e0a0be2e0637e17fa34
Edi Murwanto <edhi.uchiha@gmail.com>
2020-11-14 10:51:03 +0700

    implement navigation menu and change fragment

--- app/src/main/java/com/enigmacamp/goldmarket/SplashScreenActivity.kt
+++ app/src/main/java/com/enigmacamp/goldmarket/SplashScreenActivity.kt
@@ -1,11 +1,23 @@
 package com.enigmacamp.goldmarket
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        var handler: Handler = Handler()
+        handler.postDelayed(Runnable {
+            kotlin.run {
+
+                var intent: Intent = Intent( this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 3000)
     }
 }
