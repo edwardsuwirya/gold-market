ba00ff66ec7a8fc6612f1680705a35b3b09ea18d
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-11-14 15:35:45 +0700

    Frame Layout

--- app/src/main/java/com/enigmacamp/goldmarket/WelcomeActivity.kt
+++ app/src/main/java/com/enigmacamp/goldmarket/WelcomeActivity.kt
@@ -1,11 +1,18 @@
 package com.enigmacamp.goldmarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+    lateinit var beginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        beginButton = findViewById(R.id.welcome_button)
+        beginButton.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
