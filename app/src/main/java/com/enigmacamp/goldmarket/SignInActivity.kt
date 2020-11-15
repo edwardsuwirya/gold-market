e8cc10885fb3fb0c0520453149528dc864c73861
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-11-15 08:14:18 +0700

    Login Activity

--- app/src/main/java/com/enigmacamp/goldmarket/SignInActivity.kt
+++ app/src/main/java/com/enigmacamp/goldmarket/SignInActivity.kt
@@ -5,21 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class SignUpActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     lateinit var signInButton: Button
     lateinit var signUpButton: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-        signInButton = findViewById(R.id.signin_button)
+        setContentView(R.layout.activity_sign_in)
+        signInButton = findViewById(R.id.login_button)
         signInButton.setOnClickListener {
-            startActivity(Intent(this, SignInActivity::class.java))
-            finish()
-        }
-        signUpButton = findViewById(R.id.new_signup_button)
-        signUpButton.setOnClickListener {
 
+        }
+        signUpButton = findViewById(R.id.signup_button)
+        signUpButton.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
         }
     }
 }
