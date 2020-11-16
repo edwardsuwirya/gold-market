a1b3a88e8db6a6ab5c293ef3a7fecbe96ff21a0c
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-11-16 09:59:34 +0700

    Simple Activity Lifecycle

--- app/src/main/java/com/enigmacamp/goldmarket/SignUpActivity.kt
+++ app/src/main/java/com/enigmacamp/goldmarket/SignUpActivity.kt
@@ -2,9 +2,11 @@ package com.enigmacamp.goldmarket
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var signInButton: Button
@@ -18,11 +20,51 @@ class SignUpActivity : AppCompatActivity() {
         signInButton = findViewById(R.id.signin_button)
         signInButton.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
-            finish()
         }
         signUpButton = findViewById(R.id.new_signup_button)
         signUpButton.setOnClickListener {
             loadingDialog.show()
         }
+        print("onCreate")
+    }
+
+//    override fun onBackPressed() {
+//        val backIntent = Intent(this, WelcomeActivity::class.java)
+//        backIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+//        startActivity(backIntent)
+//    }
+
+    override fun onStart() {
+        super.onStart()
+        print("onStart, Object Id : ${this.toString()}")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        print("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        print("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        print("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        print("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        print("onDestroy")
+    }
+
+    fun print(msg: String) {
+        Log.d("Sign Up Activity State ", msg)
     }
 }
--- app/src/main/java/com/enigmacamp/goldmarket/WelcomeActivity.kt
+++ app/src/main/java/com/enigmacamp/goldmarket/WelcomeActivity.kt
@@ -1,10 +1,10 @@
 package com.enigmacamp.goldmarket
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
     lateinit var beginButton: Button
@@ -13,12 +13,44 @@ class WelcomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_welcome)
         beginButton = findViewById(R.id.welcome_button)
         beginButton.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
+            val intent = Intent(this, SignUpActivity::class.java)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            startActivity(intent)
         }
+        print("onCreate")
     }
 
-    fun onMulaiClicked(view: View) {
-        var intent: Intent = Intent( this, MainActivity::class.java)
-        startActivity(intent)
+    override fun onStart() {
+        super.onStart()
+        print("onStart, Object Id : ${this.toString()}")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        print("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        print("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        print("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        print("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        print("onDestroy")
+    }
+
+    fun print(msg: String) {
+        Log.d("Welcome Activity State ", msg)
     }
 }
