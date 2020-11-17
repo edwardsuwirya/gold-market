7221725bc96ed51e0b274fcf0667c86f1fa3434c
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-11-17 10:25:41 +0700

    Intent with data

--- app/src/main/java/com/enigmacamp/goldmarket/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/goldmarket/MainActivity.kt
@@ -7,9 +7,22 @@ import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+    private var name = ""
+    private var gold_amount = 0
+
+    lateinit var authCustomer: Customer
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        authCustomer = intent.getParcelableExtra(SignInActivity.INTENT_AUTH_CUSTOMER_KEY)
+        name = "${authCustomer.firstName} ${authCustomer.lastName}"
+        Log.d(TAG, name)
+
+        gold_amount = intent.getParcelableExtra(SignInActivity.INTENT_CUSTOMER_BALANCE)
+        Log.d(TAG, gold_amount.toString())
     }
 }
